04502807764cebd753f7ec3fefc066abdcffbbec
Dean Gerike <Dean Gerike>
2016-04-24 17:25:27 -0400

    i did stuff

---
+++ spacethingy200/Assets/stuff/code/camerafollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camerafollow : MonoBehaviour
+{
+    public GameObject follow;
+    private float lastUpdate;
+    private float wait = 2;
+    void LateUpdate()
+    {
+        if (follow != null)
+        {
+            Vector3 por = new Vector3(follow.GetComponent<Transform>().position.x, follow.GetComponent<Transform>().position.y, -30);
+            this.GetComponent<Transform>().position = por;
+        }
+        else
+        {
+            if (Time.time - lastUpdate >= wait)
+            {
+                Debug.Log("camera has no follower");
+                lastUpdate = Time.time;
+            }            
+        }
+    }
+}
